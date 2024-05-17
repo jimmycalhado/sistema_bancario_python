6e8c4cd5ba60c1b52abc3b4f5cb7651358b5c1d9
jimmycalhado <jimmyandrey.1208@gmail.com>
2024-05-17 15:36:24 -0300

    finalizado primeira parte

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -19,19 +19,44 @@ while True:
     opcao = input(menu)
     
     if opcao == "d":
-        valor = input(float())
-        if float(valor) > 0 :
-            calculo = float(valor) + saldo
-            saldo = "R$ {:,.2f}" + str(calculo)
-            print("Seu saldo agora é R$ " + str(saldo))
-        elif float(valor) < 0:
+        valor = float(input("Informe o valor do depósito: "))
+        if valor > 0 :
+            saldo += valor 
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+            print("Seu saldo agora é R$ "+ str(saldo) )
+        else:
             print("Erro ao depositar")
     
     if opcao == "s":
-        print("Saque")
+        sacar = float(input("Informe o valor de saque: "))
+        
+        excedeu_limite = limite < sacar
+        excedeu_saldo = sacar > saldo
+        excedeu_saques = numeros_saque >= LIMITE_SAQUE
+        
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+            
+        elif sacar > 0  and sacar <= saldo:
+            saldo -= sacar
+            numeros_saque += 1
+            extrato += f"Saque: R$ {sacar:.2f}\n"
+            print ( "Seu saque de "+  str(sacar) +" foi efeituado com sucesso!  Seu saldo agora é R$" + str(saldo))
+            
+        else:
+            print("Erro ao sacar")
     
     if opcao == "e":
-        print("Extrato")
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
         
     if opcao == "q":
         break
